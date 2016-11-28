3c004ff78293a80591cb3f54ac0dd897c66fa262
droeseinc <moritz.droese@googlemail.com>
2016-11-28 21:59:41 +0100

    Add files via upload

---
+++ approx.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void approx_pi() {
+	int i = 3;
+	double pi = 4;
+	double f = 4;
+	int c = 3;
+
+	while (i <= 10000) {
+
+		if (c % 2 != 0) {
+			pi -= f / i;
+			c++;
+		} else {
+			pi += f / i;
+			c++;
+		}
+		i += 2;
+
+		cout << pi << endl;
+
+	}
+
+}
+int main() {
+
+	approx_pi();
+
+	return 0;
+}
+
